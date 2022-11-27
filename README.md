f63aeb8421f24e9ae07f74db6d575effed4023fc
powcoder <powcoder@163.com>
2022-11-27 13:54:41 +0800

    CSSE1001:CSSE7030 Sudoku Puzzle A1

---
+++ README.md
@@ -0,0 +1,9 @@
+# CSSE1001:CSSE7030 Sudoku Puzzle A1
+# 加微信 powcoder
+
+# QQ 1823890830
+
+# Programming Help Add Wechat powcoder
+
+# Email: powcoder@163.com
+
---
+++ a1/a1.py
@@ -0,0 +1,45 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+"""
+CSSE1001 Assignment 1
+Semester 2, 2022
+"""
+from a1_support import *
+
+
+# Fill these in with your details
+__author__ = "Your Name"
+__email__ = "Your Email"
+__date__ = "14/07/2022"
+
+
+# Write your functions here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
